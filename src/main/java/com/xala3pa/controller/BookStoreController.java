4137c04c84fbf6f5ee7d897374da884f0a7ee29d
xala3pa <salazar3pa@gmail.com>
2017-05-02 23:05:48 +0200

    add getAllBooks endpoint

--- src/main/java/com/xala3pa/controller/BookStoreController.java
+++ src/main/java/com/xala3pa/controller/BookStoreController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+
 @RestController
 public class BookStoreController {
 
@@ -17,5 +19,11 @@ public class BookStoreController {
     @RequestMapping(method = RequestMethod.GET, value = "/books/{isbn}")
     public Book getBook(@PathVariable String isbn) {
         return bookStoreService.getBook(isbn);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/books")
+    public Collection<Book> getAllBooks(){
+        return bookStoreService.getAllBooks();
+
     }
 }
--- src/main/java/com/xala3pa/services/BookStoreService.java
+++ src/main/java/com/xala3pa/services/BookStoreService.java
@@ -3,10 +3,32 @@ package com.xala3pa.services;
 import com.xala3pa.domains.Book;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 @Service
 public class BookStoreService {
+    private Collection<Book> books = new ArrayList<>();
+
+    public  BookStoreService(){
+        Book thinking = new Book("0374275637", "Thinking, Fast and Slow",
+                "Daniel Kahneman", 10, 234 );
+        Book scala = new Book("1617290653", "Functional programing in Scala",
+                " Rúnar Bjarnason", 20, 54 );
+        Book sicp = new Book("0374275637", "SICP",
+                "Harold Abelson, Gerald Jay Sussman, Julie Sussman", 15, 24 );
+
+        books.add(thinking);
+        books.add(scala);
+        books.add(sicp);
+    }
+
     public Book getBook(String isbn) {
         return new Book("0374275637", "Thinking, Fast and Slow",
                 "Daniel Kahneman", 10, 234 );
+    }
+
+    public Collection<Book> getAllBooks() {
+        return books;
     }
 }
